d1359b5a8a73eea0bbf49812a91f5e67bf9794ff
Vladyslav Zaiets <rutova2@gmail.com>
2020-09-02 09:23:06 +0300

    cleanup in FormatterTests

---
+++ src/FeatureFlags/Constants/FeatureFlagConstants.cs
@@ -0,0 +1,65 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace FeatureFlags.Constants;
+
+/// <summary>
+/// Central location for all feature flag engine constants and configuration limits.
+/// </summary>
+public static class FeatureFlagConstants
+{
+    // Percentage rollout boundaries
+    public const int MinPercentage = 0;
+    public const int MaxPercentage = 100;
+    public const int DefaultPercentage = 50;
+
+    // Feature flag key constraints
+    public const int MaxKeyLength = 128;
+    public const int MinKeyLength = 3;
+    public const int MaxDisplayNameLength = 256;
+    public const int MaxDescriptionLength = 1000;
+
+    // Rule priorities
+    public const int MinPriority = 0;
+    public const int MaxPriority = 1000;
+    public const int DefaultPriority = 500;
+
+    // Rollout constraints
+    public const int MaxDailyIncrementPercentage = 100;
+    public const int MinDailyIncrementPercentage = 1;
+
+    // Cache and performance settings
+    public const int MaxConditionsPerRule = 50;
+    public const int MaxRulesPerFeatureFlag = 100;
+    public const int MaxVariantsPerFeatureFlag = 10;
+
+    // API response constraints
+    public const int DefaultPageSize = 20;
+    public const int MaxPageSize = 1000;
+    public const int MinPageSize = 1;
+
+    // Audit log retention
+    public const int AuditLogRetentionDays = 365;
+
+    // Condition value limits
+    public const int MaxConditionValueLength = 500;
+
+    // Timing
+    public static readonly TimeSpan DefaultCacheDuration = TimeSpan.FromMinutes(5);
+    public static readonly TimeSpan AuditLogArchiveThreshold = TimeSpan.FromDays(30);
+
+    // Default values
+    public const string DefaultConditionLogic = "AND";
+    public const bool DefaultIsEnabled = false;
+    public const bool DefaultIsActive = true;
+
+    // Error messages
+    public const string ErrorFeatureFlagNotFound = "The requested feature flag could not be found.";
+    public const string ErrorInvalidConfiguration = "The feature flag configuration is invalid.";
+    public const string ErrorRuleEvaluationFailed = "Rule evaluation encountered an error.";
+    public const string ErrorDatabaseOperation = "A database operation failed. Please try again.";
+    public const string ErrorInvalidUserContext = "The provided user context is invalid or incomplete.";
+    public const string ErrorMaxAllocationExceeded = "The total allocation percentage exceeds 100%.";
+}
---
+++ src/FeatureFlags/Models/FeatureFlag.cs
@@ -0,0 +1,72 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using FeatureFlags.Enums;
+
+namespace FeatureFlags.Models;
+
+/// <summary>
+/// Represents a feature flag with rollout strategies and targeting rules.
+/// Supports percentage-based rollouts, user targeting, and A/B testing variants.
+/// </summary>
+public class FeatureFlag
+{
+    public int Id { get; set; }
+
+    public string Key { get; set; } = string.Empty;
+
+    public string DisplayName { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public bool IsEnabled { get; set; }
+
+    public RolloutType RolloutType { get; set; } = RolloutType.Percentage;
+
+    public int? PercentageRollout { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string CreatedBy { get; set; } = string.Empty;
+
+    public string UpdatedBy { get; set; } = string.Empty;
+
+    // Navigation properties
+    public ICollection<Rule> Rules { get; set; } = new List<Rule>();
+
+    public ICollection<ABTestVariant> Variants { get; set; } = new List<ABTestVariant>();
+
+    public ICollection<AuditLog> AuditLogs { get; set; } = new List<AuditLog>();
+
+    /// <summary>
+    /// Validates the feature flag configuration ensuring consistency between rollout type and settings.
+    /// </summary>
+    public bool IsValid()
+    {
+        if (string.IsNullOrWhiteSpace(Key))
+            return false;
+
+        if (string.IsNullOrWhiteSpace(DisplayName))
+            return false;
+
+        if (RolloutType == RolloutType.Percentage && (PercentageRollout < 0 || PercentageRollout > 100))
+            return false;
+
+        if (RolloutType == RolloutType.ABTest && (!Variants?.Any() ?? true))
+            return false;
+
+        return true;
+    }
+
+    /// <summary>
+    /// Creates a snapshot of the current feature flag state for auditing purposes.
+    /// </summary>
+    public string GetSnapshot()
+    {
+        return $"Key:{Key}|IsEnabled:{IsEnabled}|RolloutType:{RolloutType}|Percentage:{PercentageRollout}|VariantCount:{Variants?.Count}";
+    }
+}
