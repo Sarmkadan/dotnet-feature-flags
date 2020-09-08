e3e4d8c52f8c95dd4196e738f0f6ea12e62e86bb
Vladyslav Zaiets <rutova2@gmail.com>
2020-09-08 14:55:01 +0300

    reshuffle CacheServiceTests a bit.
    found this while looking at something else.

---
+++ src/FeatureFlags/Configuration/DependencyInjectionExtensions.cs
@@ -0,0 +1,42 @@
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using FeatureFlags.Repository;
+using FeatureFlags.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FeatureFlags.Configuration;
+
+/// <summary>
+/// Extension methods for dependency injection configuration.
+/// Registers all feature flag services and repositories.
+/// </summary>
+public static class DependencyInjectionExtensions
+{
+    /// <summary>
+    /// Adds all feature flag services to the dependency injection container.
+    /// </summary>
+    public static IServiceCollection AddFeatureFlagServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        // Register repositories
+        services.AddScoped<IFeatureFlagRepository, FeatureFlagRepository>();
+        services.AddScoped<IAuditLogRepository, AuditLogRepository>();
+
+        // Register services
+        services.AddScoped<IFeatureFlagService, FeatureFlagService>();
+        services.AddScoped<IRuleEvaluationService, RuleEvaluationService>();
+        services.AddScoped<IPercentageRolloutService, PercentageRolloutService>();
+        services.AddScoped<IAuditLogService, AuditLogService>();
+
+        // Register configuration
+        services.Configure<FeatureFlagOptions>(configuration.GetSection("FeatureFlags"));
+
+        return services;
+    }
+}
