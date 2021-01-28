074ef13ccb5d0ba9a3f6754f0924c5d51208d695
Vladyslav Zaiets <rutova2@gmail.com>
2021-01-28 20:47:51 +0200

    Handy extensions for ConfigurationException

--- src/FeatureFlags/Exceptions/ConfigurationExceptionExtensions.cs
+++ src/FeatureFlags/Exceptions/ConfigurationExceptionExtensions.cs
@@ -2,48 +2,66 @@ using System;
 
 namespace FeatureFlags.Exceptions
 {
-    public static class ConfigurationExceptionExtensions
-    {
-        /// <summary>
-        /// Checks if the exception is a DatabaseConfigurationException
-        /// </summary>
-        public static bool IsDatabaseConfigurationError(this ConfigurationException exception)
-        {
-            return exception is DatabaseConfigurationException;
-        }
+	/// <summary>
+	/// Provides extension methods for <see cref="ConfigurationException"/> and its derived types.
+	/// </summary>
+	public static class ConfigurationExceptionExtensions
+	{
+		/// <summary>
+		/// Checks if the exception is a DatabaseConfigurationException
+		/// </summary>
+		/// <param name="exception">The exception to check. Cannot be null.</param>
+		/// <returns>True if the exception is a DatabaseConfigurationException; otherwise, false.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+		public static bool IsDatabaseConfigurationError(this ConfigurationException exception)
+		{
+			ArgumentNullException.ThrowIfNull(exception);
+			return exception is DatabaseConfigurationException;
+		}
 
-        /// <summary>
-        /// Checks if the exception is a HttpClientConfigurationException
-        /// </summary>
-        public static bool IsHttpClientConfigurationError(this ConfigurationException exception)
-        {
-            return exception is HttpClientConfigurationException;
-        }
+		/// <summary>
+		/// Checks if the exception is a HttpClientConfigurationException
+		/// </summary>
+		/// <param name="exception">The exception to check. Cannot be null.</param>
+		/// <returns>True if the exception is a HttpClientConfigurationException; otherwise, false.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+		public static bool IsHttpClientConfigurationError(this ConfigurationException exception)
+		{
+			ArgumentNullException.ThrowIfNull(exception);
+			return exception is HttpClientConfigurationException;
+		}
 
-        /// <summary>
-        /// Gets the root cause message by traversing the inner exception chain
-        /// </summary>
-        public static string GetRootCauseMessage(this ConfigurationException exception)
-        {
-            var current = exception;
-            while (current.InnerException is ConfigurationException innerConfigException)
-            {
-                current = innerConfigException;
-            }
-            return current.Message;
-        }
+		/// <summary>
+		/// Gets the root cause message by traversing the inner exception chain
+		/// </summary>
+		/// <param name="exception">The exception to analyze. Cannot be null.</param>
+		/// <returns>The message of the root ConfigurationException in the inner exception chain.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+		public static string GetRootCauseMessage(this ConfigurationException exception)
+		{
+			ArgumentNullException.ThrowIfNull(exception);
 
-        /// <summary>
-        /// Gets a formatted error message including error code if available
-        /// </summary>
-        public static string ToFormattedMessage(this ConfigurationException exception)
-        {
-            var message = exception.Message;
-            if (!string.IsNullOrEmpty(exception.ErrorCode))
-            {
-                message = $"[{exception.ErrorCode}] {message}";
-            }
-            return message;
-        }
-    }
+			var current = exception;
+			while (current.InnerException is ConfigurationException innerConfigException)
+			{
+				current = innerConfigException;
+			}
+			return current.Message;
+		}
+
+		/// <summary>
+		/// Gets a formatted error message including error code if available
+		/// </summary>
+		/// <param name="exception">The exception to format. Cannot be null.</param>
+		/// <returns>A formatted message containing the error code (if available) and the exception message.</returns>
+		/// <exception cref="ArgumentNullException">Thrown when <paramref name="exception"/> is null.</exception>
+		public static string ToFormattedMessage(this ConfigurationException exception)
+		{
+			ArgumentNullException.ThrowIfNull(exception);
+
+			return !string.IsNullOrEmpty(exception.ErrorCode)
+				? $"[{exception.ErrorCode}] {exception.Message}"
+				: exception.Message;
+		}
+	}
 }
