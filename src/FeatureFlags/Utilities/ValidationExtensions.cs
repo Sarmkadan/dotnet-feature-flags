e46b0476b55e11d92f197de7aa2dc97d9362f4d7
Vladyslav Zaiets <rutova2@gmail.com>
2021-02-12 13:20:49 +0200

    More ValidationExtensions extension methods

--- src/FeatureFlags/Utilities/ValidationExtensions.cs
+++ src/FeatureFlags/Utilities/ValidationExtensions.cs
@@ -1,4 +1,5 @@
 #nullable enable
+
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
@@ -15,6 +16,9 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if collection is null or empty.
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to check.</param>
+    /// <returns>True if the collection is null or empty; otherwise, false.</returns>
     public static bool IsNullOrEmpty<T>(this IEnumerable<T>? collection)
     {
         return collection?.Any() != true;
@@ -23,6 +27,10 @@ public static class ValidationExtensions
     /// <summary>
     /// Throws ArgumentException if collection is null or empty.
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to check.</param>
+    /// <param name="paramName">Name of the parameter being validated.</param>
+    /// <exception cref="ArgumentException">Thrown when the collection is null or empty.</exception>
     public static void ThrowIfNullOrEmpty<T>(this IEnumerable<T>? collection, string paramName)
     {
         if (collection.IsNullOrEmpty())
@@ -34,6 +42,9 @@ public static class ValidationExtensions
     /// <summary>
     /// Throws ArgumentException if string is null or whitespace.
     /// </summary>
+    /// <param name="value">The string value to check.</param>
+    /// <param name="paramName">Name of the parameter being validated.</param>
+    /// <exception cref="ArgumentException">Thrown when the string is null or whitespace.</exception>
     public static void ThrowIfNullOrWhiteSpace(this string? value, string paramName)
     {
         if (string.IsNullOrWhiteSpace(value))
@@ -45,6 +56,8 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if string is a valid non-negative integer.
     /// </summary>
+    /// <param name="input">The string to validate.</param>
+    /// <returns>True if the string is a valid non-negative integer; otherwise, false.</returns>
     public static bool IsValidNonNegativeInteger(this string? input)
     {
         return int.TryParse(input, out var value) && value >= 0;
@@ -53,6 +66,8 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if string is a valid percentage (0-100).
     /// </summary>
+    /// <param name="input">The string to validate.</param>
+    /// <returns>True if the string is a valid percentage; otherwise, false.</returns>
     public static bool IsValidPercentage(this string? input)
     {
         return int.TryParse(input, out var value) && value >= 0 && value <= 100;
@@ -61,6 +76,10 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if integer is within specified range (inclusive).
     /// </summary>
+    /// <param name="value">The value to check.</param>
+    /// <param name="min">The minimum value (inclusive).</param>
+    /// <param name="max">The maximum value (inclusive).</param>
+    /// <returns>True if the value is within range; otherwise, false.</returns>
     public static bool IsInRange(this int value, int min, int max)
     {
         return value >= min && value <= max;
@@ -69,6 +88,8 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if integer is a valid percentage (0-100).
     /// </summary>
+    /// <param name="value">The value to check.</param>
+    /// <returns>True if the value is a valid percentage; otherwise, false.</returns>
     public static bool IsValidPercentage(this int value)
     {
         return value >= 0 && value <= 100;
@@ -77,6 +98,9 @@ public static class ValidationExtensions
     /// <summary>
     /// Throws ArgumentException if integer is not a valid percentage.
     /// </summary>
+    /// <param name="value">The value to validate.</param>
+    /// <param name="paramName">Name of the parameter being validated.</param>
+    /// <exception cref="ArgumentException">Thrown when the value is not a valid percentage.</exception>
     public static void ThrowIfNotValidPercentage(this int value, string paramName)
     {
         if (!value.IsValidPercentage())
@@ -88,6 +112,9 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if collection contains any duplicate elements.
     /// </summary>
+    /// <typeparam name="T">The type of elements in the collection.</typeparam>
+    /// <param name="collection">The collection to check for duplicates.</param>
+    /// <returns>True if duplicates exist; otherwise, false.</returns>
     public static bool HasDuplicates<T>(this IEnumerable<T> collection)
     {
         var set = new HashSet<T>();
@@ -97,6 +124,10 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if string length is between min and max (inclusive).
     /// </summary>
+    /// <param name="input">The string to validate.</param>
+    /// <param name="minLength">The minimum length (inclusive).</param>
+    /// <param name="maxLength">The maximum length (inclusive).</param>
+    /// <returns>True if the string length is valid; otherwise, false.</returns>
     public static bool IsLengthValid(this string? input, int minLength, int maxLength)
     {
         return !string.IsNullOrEmpty(input) &&
@@ -107,28 +138,29 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if string matches a simple key format (alphanumeric + underscore/dash).
     /// </summary>
-    public static bool IsValidKeyFormat(this string? input)
-    {
-        if (string.IsNullOrWhiteSpace(input))
-        {
-            return false;
-        }
-
-        return System.Text.RegularExpressions.Regex.IsMatch(input, @"^[a-zA-Z0-9_-]+$");
-    }
+    /// <param name="input">The string to validate.</param>
+    /// <returns>True if the string matches the key format; otherwise, false.</returns>
+    public static bool IsValidKeyFormat(this string? input) =>
+        !string.IsNullOrWhiteSpace(input) &&
+        System.Text.RegularExpressions.Regex.IsMatch(input, @"^[a-zA-Z0-9_-]+$");
 
     /// <summary>
     /// Checks if string contains only alphanumeric characters.
     /// </summary>
+    /// <param name="input">The string to validate.</param>
+    /// <returns>True if the string is alphanumeric; otherwise, false.</returns>
     public static bool IsAlphanumeric(this string? input)
     {
-        return !string.IsNullOrEmpty(input) &&
-               input.All(char.IsLetterOrDigit);
+        return !string.IsNullOrEmpty(input) && input.All(char.IsLetterOrDigit);
     }
 
     /// <summary>
     /// Validates that object is not null, throws ArgumentNullException if it is.
     /// </summary>
+    /// <typeparam name="T">The type of the object.</typeparam>
+    /// <param name="obj">The object to validate.</param>
+    /// <param name="paramName">Name of the parameter being validated.</param>
+    /// <exception cref="ArgumentNullException">Thrown when the object is null.</exception>
     public static void ThrowIfNull<T>(this T? obj, string paramName) where T : class
     {
         if (obj is null)
@@ -140,6 +172,9 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if value is the default value for its type.
     /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="value">The value to check.</param>
+    /// <returns>True if the value is default; otherwise, false.</returns>
     public static bool IsDefault<T>(this T value) where T : struct
     {
         return Equals(value, default(T));
@@ -148,6 +183,8 @@ public static class ValidationExtensions
     /// <summary>
     /// Checks if GUID is empty (all zeros).
     /// </summary>
+    /// <param name="value">The GUID to check.</param>
+    /// <returns>True if the GUID is empty; otherwise, false.</returns>
     public static bool IsEmpty(this Guid value)
     {
         return value == Guid.Empty;
@@ -156,6 +193,9 @@ public static class ValidationExtensions
     /// <summary>
     /// Throws ArgumentException if GUID is empty.
     /// </summary>
+    /// <param name="value">The GUID to validate.</param>
+    /// <param name="paramName">Name of the parameter being validated.</param>
+    /// <exception cref="ArgumentException">Thrown when the GUID is empty.</exception>
     public static void ThrowIfEmpty(this Guid value, string paramName)
     {
         if (value.IsEmpty())
