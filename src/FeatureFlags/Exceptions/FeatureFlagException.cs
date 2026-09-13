ff88e139ebac7d0cac4392a500abf8c99efeb591
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-13 08:01:47 +0000

    chore: apply workspace updates

--- src/FeatureFlags/Exceptions/FeatureFlagException.cs
+++ src/FeatureFlags/Exceptions/FeatureFlagException.cs
@@ -12,20 +12,40 @@ namespace FeatureFlags.Exceptions;
 public class FeatureFlagException : Exception {
     public string? ErrorCode { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureFlagException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public FeatureFlagException(string message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureFlagException"/> class with a specified error message and error code.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="errorCode">The error code associated with the exception.</param>
     public FeatureFlagException(string message, string errorCode) : base(message)
     {
         ErrorCode = errorCode;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureFlagException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or <c>null</c> if no inner exception is specified.</param>
     public FeatureFlagException(string message, Exception innerException)
         : base(message, innerException)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FeatureFlagException"/> class with a specified error message, error code, and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="errorCode">The error code associated with the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or <c>null</c> if no inner exception is specified.</param>
     public FeatureFlagException(string message, string errorCode, Exception innerException)
         : base(message, innerException)
     {
