a5a44aab23b7a298884f5ce9f1bbd6d587bb735f
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-16 23:02:51 +0000

    chore: apply workspace updates

--- src/FeatureFlags/Exceptions/ValidationException.cs
+++ src/FeatureFlags/Exceptions/ValidationException.cs
@@ -14,15 +14,29 @@ public class ValidationException : FeatureFlagException
 {
     public Dictionary<string, string> Errors { get; } = new();
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the validation error.</param>
     public ValidationException(string message) : base(message, "VALIDATION_ERROR")
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationException"/> class with a specified error message and a collection of field errors.
+    /// </summary>
+    /// <param name="message">The message that describes the validation error.</param>
+    /// <param name="errors">A dictionary of field names to their corresponding validation error messages.</param>
     public ValidationException(string message, Dictionary<string, string> errors) : base(message, "VALIDATION_ERROR")
     {
         Errors = errors;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ValidationException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The message that describes the validation error.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception.</param>
     public ValidationException(string message, Exception innerException) : base(message, "VALIDATION_ERROR", innerException)
     {
     }
